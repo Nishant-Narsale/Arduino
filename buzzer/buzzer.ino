f11561ed3f95d5989a2caa0f717b4b92f4580f5b
Nishant <nishantnarsale33@gmail.com>
2022-08-22 17:27:49 +0530

    active buzzer

--- buzzer/buzzer.ino
+++ buzzer/buzzer.ino
@@ -1,11 +1,32 @@
-
+int buzzPin=9;
+int dt=500;
+int dt2=50;
+int potval=0;
+int potPin=A2;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(buzzPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(potPin, INPUT);
+}
 
+void buzz(){
+    digitalWrite(buzzPin, HIGH);
+    delay(dt);
+    digitalWrite(buzzPin, LOW);
+    delay(dt);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  potval = analogRead(potPin);
+  Serial.println(potval);
+
+  while(potval>500){
+    buzz();
+    potval = analogRead(potPin);
+    Serial.println(potval);
+  }
 
 }
